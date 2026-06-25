5c4db3ee253d85f3cf30f13102fbd28e6da65a28
Jakob Hubbard <jakobhubbard778@gmail.com>
2026-06-25 00:53:48 -0400

    Add flat & flatMap polyfills

--- src/polyfills/array.ts
+++ src/polyfills/array.ts
@@ -7,3 +7,37 @@ if (!Array.prototype.includes) {
         return false;
     }
 }
+
+if (!Array.prototype.flatMap) {
+    Array.prototype.flatMap = function<T, U, This = undefined>(
+        callback: (this: This, value: T, index: number, array: T[]) => U | ReadonlyArray<U>,
+        thisArg: This
+    ) {
+        return this.reduce((acc, current, index, array) => {
+            const mapped = callback.call(thisArg, current, index, array);
+            return acc.concat(mapped);
+        }, []);
+    };
+}
+
+if (!Array.prototype.flat) {
+  Array.prototype.flat = function <A, D extends number = 1>(
+    this: A,
+    depth?: D
+  ): FlatArray<A, D>[] {
+    const depthNum = (depth ?? 1) as number;
+
+    const flatten = (arr: any[], currentDepth: number): any[] => {
+      return arr.reduce((acc: any[], val: any) => {
+        if (Array.isArray(val) && currentDepth < depthNum) {
+          acc.push(...flatten(val, currentDepth + 1));
+        } else {
+          acc.push(val);
+        }
+        return acc;
+      }, []);
+    };
+
+    return flatten(this as any[], 0);
+  };
+}
